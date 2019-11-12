9bd8ca002a243e0d45c934e587e67028f8550c50
superyaz <yacamocursos@hotmail.com>
2019-11-12 00:50:07 -0500

    Se modifica el archivo index.html ya que tenia un nombre incorrecto y no permitia el deploy desde Netlify

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// 'use strict'
+'use strict'
 
-// function hypotenuse (a, b){
-//     a = prompt("Ingrese un numero por favor: ");
-//     b = prompt("Ingrese un numero por favor: ");
-//      let operacion = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-//     document.getElementById("resultado").innerHTML = operacion;
-// }
-// hypotenuse();
+function hypotenuse(a, b) {
+    a = prompt("Ingrese un numero por favor: ");
+    b = prompt("Ingrese un numero por favor: ");
+    let operacion = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    document.getElementById("resultado").innerHTML = operacion;
+}
+hypotenuse();
