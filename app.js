e70f3b19915d88b9681950271274f42021f189b9
superyaz <yacamocursos@hotmail.com>
2019-11-12 00:53:16 -0500

    Se modifica el archivo app.js para cambiar la forma en que se seolicitan los números de entrada

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict'
 
 function hypotenuse(a, b) {
-    a = prompt("Ingrese un numero por favor: ");
-    b = prompt("Ingrese un numero por favor: ");
+    a = prompt("Ingrese un primer número por favor: ");
+    b = prompt("Ingrese un segundo número por favor: ");
     let operacion = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
     document.getElementById("resultado").innerHTML = operacion;
 }
